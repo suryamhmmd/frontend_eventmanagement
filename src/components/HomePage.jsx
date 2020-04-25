7e2b883700ac725cef0cbdcbc1f66af11c40e8db
azmi <ihsanazmiii@gmail.com>
2020-04-26 02:11:25 +0700

    finished function register

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Header from './Header'
+import { Link } from 'react-router-dom'
 
 
 export class HomePage extends Component {
@@ -13,8 +14,12 @@ export class HomePage extends Component {
                     <div className="path-31 d-flex flex-column justify-content-between align-items-center">
                         <h1 className="text-center mt-5">Aplikasi yang membantu dalam mengatur sebuah event</h1>
                         <div className="d-flex flex-row button-homepage">
-                            <button className="mx-5 button">Register</button>
-                            <button className="mx-4 button">Login</button>
+                            <Link to="/register">
+                                <button className="mx-5 button">Register</button>
+                            </Link>
+                            <Link to="/login">
+                                <button className="mx-4 button">Login</button>
+                            </Link>
                         </div>
                     </div>
                 </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Header from './Header'
-import {Link} from 'react-router-dom'
+import {Link, Redirect} from 'react-router-dom'
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Swal from 'sweetalert2'
 
+import axios from '../config/axios'
 
 const GreenCheckbox = withStyles({
     root: {
@@ -18,22 +20,107 @@ const GreenCheckbox = withStyles({
   })((props) => <Checkbox color="default" {...props} />);
 
 export class Register extends Component {
+
+    state={
+        formRegister:{
+            nama:'',
+            email:'',
+            password:'',
+            confirmPassword:'',
+        },
+        status:false
+    }
+
+    handleChange = (event)=>{
+        let {formRegister} = this.state
+        formRegister[event.target.name] = event.target.value
+        this.setState({
+            formRegister
+        })
+    }
+
+    login = ()=>{
+        let {nama, email, password} = this.state.formRegister
+
+        if(!nama || !email || !password){
+            Swal.fire({
+                icon:'error',
+                title:'Field Empty',
+                text:'Fields Cannot be Empty'
+            })
+            return
+        }
+
+        let data = {nama, email, password}
+        
+        axios.post(`/user/register`, data)
+        .then(res=>{
+            console.log(res.data)
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Registration Success',
+                showConfirmButton: false,
+                timer: 1500
+            })
+            this.setState({status:true})
+
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+
+    renderTextField = (name, label, type)=>{
+        return(
+            <TextField 
+                style={{backgroundColor:'white'}} 
+                fullWidth 
+                className="rounded mt-3"
+                name={name}
+                size="small" 
+                label={label} 
+                type={type}
+                variant="filled" 
+                onChange={this.handleChange}
+            />
+        )
+    }
+
     render() {
+
+        if(this.state.status){
+            return <Redirect to="/"/>
+        }
+
         return (
             <div className="home-page">
                 <Header/>
                 <div className="d-flex justify-content-center">
                     <div className="frame-register d-flex flex-column alignite px-5 align-items-center">
                         <h1 className="text-center">Event Management User Register</h1>
-                        <TextField style={{backgroundColor:'white'}} fullWidth className="rounded mt-3" size="small" label="Full Name" variant="filled" />
-                        <TextField style={{backgroundColor:'white'}} fullWidth className="rounded mt-3" size="small" label="Email" variant="filled" />
-                        <TextField style={{backgroundColor:'white'}} fullWidth className="rounded mt-3" size="small" label="Password" variant="filled" />
-                        <TextField style={{backgroundColor:'white'}} fullWidth className="rounded mt-3" size="small" label="Confirm Password" variant="filled" />
+                        {this.renderTextField('nama', 'Full Name')}
+                        {this.renderTextField('email', 'Email')}
+                        {this.renderTextField('password', 'Password', 'password')}
+
+                        <TextField 
+                            style={{backgroundColor:'white'}} 
+                            fullWidth 
+                            className="rounded mt-3"
+                            name="confirmPassword"
+                            size="small" 
+                            label="Confirm Password"
+                            type="password"
+                            variant="filled" 
+                            onChange={this.handleChange}
+                            error={this.state.formRegister.password !== this.state.formRegister.confirmPassword ? true : false}
+                        />
+
                         <FormControlLabel
                             control={<GreenCheckbox name="infoExpectStrengths" value="accept"/>}
                             label="I accept terms of use" className="align-self-start" style={{color:'white'}}
                         />
-                        <button className="btn btn-light">Sign Up</button>
+                        <button onClick={this.login} disabled={this.state.formRegister.password !== this.state.formRegister.confirmPassword ? true : false} className="btn btn-light">Sign Up</button>
                         <p className="mt-3 text-white">have a account? <Link className="text-white" to="/login"><u>Sign In</u></Link></p>
                     </div>
                 </div>
