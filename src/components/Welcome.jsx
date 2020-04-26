22f06108309a208902c1c3e5ccc27da9d1f082a6
azmi <ihsanazmiii@gmail.com>
2020-04-26 16:17:14 +0700

    finish page welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -13,13 +13,13 @@ export class Welcome extends Component {
                         <div className="path-31 d-flex flex-column justify-content-around align-items-center">
                             <h1 className="text-center mt-5">Welcome, {this.props.login.nama}</h1>
                             <div className="d-flex flex-row button-homepage">
-                                <Link to="/register">
+                                <Link to="/dashboard">
                                     <div className="btn btn-outline-light mx-4">
                                         <DashboardIcon  style={{width:200, height:200}}/>
                                         <h3>Dashboard</h3>
                                     </div>
                                 </Link>
-                                <Link to="/login">
+                                <Link to="/create_event">
                                     <div className="btn btn-outline-light mx-4">
                                         <InsertInvitationIcon style={{width:200, height:200}}/>
                                         <h3>Create Event</h3>
